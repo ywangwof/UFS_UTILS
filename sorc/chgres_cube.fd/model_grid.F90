9c7daaa35d6c4fe525b054fa93447a2062f4d991
gerard ketefian <gerard.ketefian@noaa.gov>
2019-06-24 14:17:37 -0600

    Remove unused variable from USE statement.

--- sorc/chgres_cube.fd/model_grid.F90
+++ sorc/chgres_cube.fd/model_grid.F90
@@ -1088,7 +1088,7 @@
  use netcdf
  use program_setup, only       : mosaic_file_target_grid, &
                                  orog_dir_target_grid,    &
-                                 orog_files_target_grid, phys_suite, &
+                                 orog_files_target_grid,  &
                                  numsoil_out
 
  implicit none
