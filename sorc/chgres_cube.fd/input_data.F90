6ffe837099690976b4bfef8bc4a6c2b5e3636c3e
Jeff.Beck <Jeff.Beck@noaa.gov>
2020-03-20 01:15:07 +0000

    When using HRRRX or RAPX data for ICs, check if geogrid file exists, and exit if not.

--- sorc/chgres_cube.fd/input_data.F90
+++ sorc/chgres_cube.fd/input_data.F90
@@ -4647,6 +4647,8 @@ if (localpet == 0) then
        endif
        print*, "CLOSE GEOGRID FILE "
        iret = nf90_close(ncid2d)
+     else
+     call error_handler("COULD NOT FIND GEOGRID FILE. PLEASE CHECK PATH. EXITING.")
      endif
    endif
    
